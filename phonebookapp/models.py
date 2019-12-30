41e48ddf7fc48db9f4e1d69fa3545e58086ea0cd
gstark0 <gstark0@icloud.com>
2019-12-30 16:33:40 +0100

    Models created

--- phonebookapp/models.py
+++ phonebookapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Person(models.Model):
+    name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
+ 
+class Phone(models.Model):
+    person = models.ForeignKey(Person, editable=False, on_delete=models.PROTECT)
+    phone = models.CharField(max_length=50)
+ 
+class Email(models.Model):
+    person = models.ForeignKey(Person, editable=False, on_delete=models.PROTECT)
+    email = models.EmailField()
