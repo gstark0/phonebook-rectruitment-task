60b90dd27d8ae08bd232317f42a6e95b2daa8cf7
gstark0 <gstark0@icloud.com>
2020-01-02 12:23:32 +0100

    Edit and delete person views

--- phonebookapp/urls.py
+++ phonebookapp/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<int:person_id>/edit/', views.editPerson, name='edit'),
+    path('<int:person_id>/delete/', views.deletePerson, name='edit')
 ]
--- phonebookapp/views.py
+++ phonebookapp/views.py
@@ -5,3 +5,9 @@ from django.template import loader
 # Create your views here.
 def index(request):
     return render(request, 'phonebookapp/index.html')
+
+def editPerson(request, person_id):
+    return HttpResponse(person_id)
+
+def deletePerson(request, person_id):
+    return HttpResponse(person_id)
