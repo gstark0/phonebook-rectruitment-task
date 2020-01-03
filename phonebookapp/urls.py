4bd3c9324e0ab9b48fe507be181de31af18df9cd
gstark0 <gstark0@icloud.com>
2020-01-03 01:09:05 +0100

    Edit person

--- phonebookapp/urls.py
+++ phonebookapp/urls.py
@@ -4,7 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:person_id>/edit/', views.editPerson, name='edit'),
-    path('<int:person_id>/add/', views.addPerson, name='add'),
-    path('<int:person_id>/delete/', views.deletePerson, name='edit')
+    path('<int:person_id>/edit/', views.editPerson, name='edit_person'),
+    path('<int:person_id>/add_phone/', views.addPhone, name='add_phone'),
+    path('<int:person_id>/add_email/', views.addPhone, name='add_email'),
+    path('add/', views.addPerson, name='add_person'),
+    path('<int:person_id>/delete/', views.deletePerson, name='delete_person')
 ]
--- phonebookapp/views.py
+++ phonebookapp/views.py
@@ -1,16 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.decorators.http import require_http_methods
+from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 from django.template import loader
 
+from .models import Person, Phone, Email
+
 # Create your views here.
 def index(request):
-    return render(request, 'phonebookapp/index.html')
+    people = Person.objects.all()
+    phones = Phone.objects.all()
+    return render(request, 'phonebookapp/index.html', {'people': people})
 
-def addPerson(request, person_id):
+@require_http_methods(['POST'])
+def addPerson(request):
+    name = request.POST.get('name')
+    surname = request.POST.get('surname')
+
+    print(name, surname)
+
     return render(request, 'phonebookapp/add.html')
 
+def addPhone(request):
+    return HttpResponse('Works')
+
+def addEmail(request):
+    return HttpResponse('Works')
+
+@csrf_exempt
+@require_http_methods(['GET', 'POST'])
 def editPerson(request, person_id):
-    return render(request, 'phonebookapp/edit.html')
+    person = get_object_or_404(Person, pk=person_id)
+
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        surname = request.POST.get('surname')
+        
+        person.name = name
+        person.surname = surname
+
+        person.save()
+
+        return redirect('/phonebook')
+    else:
+        return render(request, 'phonebookapp/edit.html', {'name': person.name, 'surname': person.surname})
 
 def deletePerson(request, person_id):
     return HttpResponse(person_id)
