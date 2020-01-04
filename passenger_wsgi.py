065db23f0f85b4b12dab011f9aa67bbdd960173a
gstark0 <gstark0@icloud.com>
2020-01-04 13:16:19 +0100

    Change app name in passenger_wsgi.py

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,7 +1,7 @@
 import sys, os
 
 sys.path.append(os.getcwd())
-os.environ['DJANGO_SETTINGS_MODULE'] = 'phonebookapp.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'phonebook.settings'
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
